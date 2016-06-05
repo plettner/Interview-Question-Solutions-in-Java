9e6f51499e15dec52bc0a320392912498fff7c34
Marvin Plettner <marvin@plettner.com>
2016-06-05 00:20:55 -0700

    Updated comments on RemoveRepeatingCharacters problem.

--- src/main/java/strings/RemoveRepeatingCharacters.java
+++ src/main/java/strings/RemoveRepeatingCharacters.java
@@ -4,14 +4,23 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * The question is to remove all repeated characters, leaving only one 
- * instance of each character in the string.  Two solutions found online.
- * One is to sort the string, then delete any duplicates.  The second is
- * to use a hash map of some sort to track duplicate characters.
+ * The question is to remove all repeated characters, leaving only one instance
+ * of each character in the string. Two solutions found online. One is to sort
+ * the string, then delete any duplicates. The second is to use a hash map of
+ * some sort to track duplicate characters.
  */
 public class RemoveRepeatingCharacters {
 
-	public char [] removeRepeatingChars(char [] array) {
+	/**
+	 * This solution to the problem uses a map to track which characters have
+	 * been duped. This retains the original ordering of the character array
+	 * once the dupes are removed.
+	 * 
+	 * @param array
+	 *            array of characters, possibly with duplicates
+	 * @return an array with any duplicate characters removed
+	 */
+	public char[] removeRepeatingChars(char[] array) {
 		Map<Character, Integer> map = new HashMap<>();
 
 		int putIndex = 0;
@@ -21,13 +30,13 @@ public class RemoveRepeatingCharacters {
 			if (count == null) {
 				map.put(ch, 1);
 				array[putIndex++] = array[getIndex];
-			} 
+			}
 		}
-		
-		// We can't null terminate a string in Java.  So we'll cheat
+
+		// We can't null terminate a string in Java. So we'll cheat
 		// and create a new, proper length array and copy the results
 		// into it.
-		char [] newArray = new char [putIndex];
+		char[] newArray = new char[putIndex];
 		for (int index = 0; index < newArray.length; index++) {
 			newArray[index] = array[index];
 		}
